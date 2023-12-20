a685e650a96665658134b9b2cf7edff247af39a3
gaspar burgi <gasparburgi49@gmail.com>
2023-12-19 22:25:45 -0300

    first vgg16 declaration

--- vgg16.py
+++ vgg16.py
@@ -2,6 +2,7 @@
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras import optimizers
 from preprocess import balance_datasets
 from utils import *
 import os
@@ -26,16 +27,25 @@ def add_last_layers(num_classes=2):
     model = vgg16(input_shape=(255,255,3))
     model = set_non_trainable(model)
     flattening_layer=layers.Flatten()
-    dense_layer = layers.Dense(64,activation='relu')
+    dense_layer_1 = layers.Dense(64,activation='relu')
+    dense_layer_2 = layers.Dense(128,activation='relu')
+
     predict_layer = layers.Dense(num_classes,activation='softmax')
 
     model = Sequential([
         model,
         flattening_layer,
-        dense_layer,
+        dense_layer_1,
+        dense_layer_2,
         predict_layer
     ])
     return model
-model = add_last_layers()
+def build_model(num_classes=2):
+    # $CHALLENGIFY_BEGIN
+    model = add_last_layers(num_classes=num_classes)
 
-print(model.summary())
+    opt = optimizers.Adam(learning_rate=1e-4)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+    return model
